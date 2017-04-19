9e733855a10189b5c293e21c18e5c4ee40703d78
slavizdravkov <slavizdravkov@gmail.com>
2017-04-19 21:32:06 +0300

    Implement user registrations

--- src/OnlineShopBundle/Controller/UserController.php
+++ src/OnlineShopBundle/Controller/UserController.php
@@ -9,7 +9,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserController extends Controller
 {
